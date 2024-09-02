2e92af4f11cc0fde6695530a780403cb0c77e698
thuynguyentd37 <ntdt37@gmail.com>
2024-09-02 15:29:34 +0700

    update 15:40

--- src/main/java/uk/co/automationtesting/liveproject1/App.java
+++ src/main/java/uk/co/automationtesting/liveproject1/App.java
@@ -11,5 +11,10 @@ public class App
         System.out.println( "Hello World2!" );
         System.out.println( "Hello World1!" );
         System.out.println( "Hello World3!" );
+        System.out.println( "Hello World2!" );
+        System.out.println( "Hello World1!" );
+        System.out.println( "Hello World3!" );
+        //fhgsdh
+        System.out.println( "I'm in develop branch" ); 
     }
 }
